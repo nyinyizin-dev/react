3d82667b95eaa736c42ece189d4166d11aafc902
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-07 12:27:53 +0900

    add and remove method

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-function Item({ item }) {
-  return <li> {item.name}</li>;
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+function Item({ item, remove }) {
+  return (
+    <li> 
+      {item.name}  {" "}{" "}
+       <button onClick={()=>remove(item.id)}>remove</button>
+    </li>
+  );
 }
 
 export default function App() {
-  const data = [
-    { name: "Apple", done: true },
-    { name: "Orange", done: true },
-    { name: "Mango", done: false },
-    { name: "Banana", done: false },
-  ];
+  const [data, setData] = useState([
+    { id: 4, name: "Apple", done: true },
+    { id: 3, name: "Orange", done: true },
+    { id: 2, name: "Mango", done: false },
+    { id: 1, name: "Banana", done: false },
+  ]);
+
+  const add = (name) => {
+    const id = data[0].id + 1;
+    setData([{ id, name: "New Item", done: false }, ...data]);
+  };
+
+  const remove = id => {
+    setData(data.filter(item => item.id != id))
+  }
 
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello React ({data.length})</h1>
+      <button onClick={add}>Add</button>
+
       <ul>
         {data.map((item) => {
-          return <Item item={item} key={item.name} />;
+          return <Item item={item} key={item.id} remove={remove} />;
         })}
-
-        {data.map((item) => (
-          <Item item={item} key={item.name} />
-        ))}
-
-        {data.map((item) => (
-          <Item item={item} key={item.name} />
-        ))}
       </ul>
     </div>
   );
