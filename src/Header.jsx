3fef714f423f7cc100d4efd8e741e23d248f1d72
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-14 12:20:11 +0900

    Theme mode added on HeadBar using useContext,useMemo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,32 @@
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, Badge, IconButton } from "@mui/material";
 
-export default function Header() {
-    return <AppBar position="static">
-        <Toolbar>
-            <Typography>Todo</Typography>
-        </Toolbar>
+import {
+  LightMode as LightModeIcon,
+  DarkMode as DarkModeIcon,
+} from "@mui/icons-material";
+
+import { useApp } from "./AppProvider";
+
+export default function Header({ count }) {
+  const { mode, setMode } = useApp();
+
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Badge badgeContent={count} color="error">
+          <Typography>Todo</Typography>
+        </Badge>
+
+        {mode == "dark" ? (
+          <IconButton color="inherit" onClick={() => setMode("light")}>
+            <LightModeIcon />
+          </IconButton>
+        ) : (
+          <IconButton color="inherit" onClick={() => setMode("dark")}>
+            <DarkModeIcon />
+          </IconButton>
+        )}
+      </Toolbar>
     </AppBar>
+  );
 }
